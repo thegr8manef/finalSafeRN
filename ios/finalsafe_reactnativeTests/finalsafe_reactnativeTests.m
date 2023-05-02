968feeb64a1d1db43224b27fa11739f9b8ffc0f0
kchaou-fakhri <kchaoufakhri@gmail.com>
2023-05-02 15:03:40 +0100

    feat() : Change APplication ID - Change application id to com.mobelite.FinalSafeRNAndroid

--- ios/finalsafe_reactnativeTests/finalsafe_reactnativeTests.m
+++ ios/finalsafe_reactnativeTests/finalsafe_reactnativeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface finalsafe_reactnativeTests : XCTestCase
+@interface finalsafereactnativeTests : XCTestCase
 
 @end
 
-@implementation finalsafe_reactnativeTests
+@implementation finalsafereactnativeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
