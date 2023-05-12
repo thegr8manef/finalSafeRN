90663478c4927bcc896b12ee5bae186acf426dfd
thegr8manef <jedidimanef@gmail.com>
2023-05-12 12:42:20 +0100

    feature/splashScreen/completed

--- src/screens/Auth/Splash/SplashStyles.js
+++ src/screens/Auth/Splash/SplashStyles.js
@@ -3,11 +3,13 @@ import {StyleSheet} from 'react-native';
 export default styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: colors.white,
         width: '100%',
         height: '100%',
+      },
+      container_logo_splash:{
+        flex: 3,
+        alignItems: 'center'
       },
       logo_splash: {
         marginTop:110,
@@ -15,34 +17,24 @@ export default styles = StyleSheet.create({
         height: 200,
         resizeMode: 'stretch',
       },
-      statusBar: {
-        width: '100%',
-        height: '100%',
-        backgroundColor: colors.primary,
-      },
       logo_eiffage: {
-    
         width: 130,
         height: 40,
         resizeMode: 'stretch',
         alignSelf:'center',
-        justifyContent:'flex-end'
-      },
-      squares_splash:{
-        flex:0.2,
-        backgroundColor:'green',
-        alignSelf:'flex-end',
-        position:'absolute',
         justifyContent:'flex-end',
-        alignItems:'flex-end'
-
+      },
+      container_squares_splash_img:{
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        padding:5
       },
       squares_splash_img:{
         width: 20,
         height: 100,
         resizeMode: 'stretch',
         position:'absolute',
-        backgroundColor:'green',
         alignSelf:'flex-end',
         position:'absolute',
         justifyContent:'flex-end',
