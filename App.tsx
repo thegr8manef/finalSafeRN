cdad5d71b95748777112a662c3895c8b83fad615
thegr8manef <jedidimanef@gmail.com>
2023-05-15 12:53:29 +0100

    AddScreenProfile

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 import Login from './src/screens/Auth/Login'
 import colors from './src/assets/colors'
 import SplashScreen from './src/screens/Auth/Splash/SplashScreen'
+import ProfileScreen from './src/screens/Auth/Profile/ProfileScreen'
 
 export default function App() {
   return (
     <SafeAreaView style={{flex : 1}}>
       <StatusBar backgroundColor={colors.primary} />
-      <SplashScreen/>
+      <ProfileScreen/>
     </SafeAreaView>
   )
 }
---
+++ src/screens/Auth/Profile/ProfileStyle.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export default styles = StyleSheet.create({
+    header_container:{
+        flex:0.5,
+        flexDirection:'row-reverse',
+        height:1,
+    },
+    button_container:{
+        backgroundColor:colors.primary,
+        flex:1,
+        height:50
+    },
+    text_container:{
+        flex:3,
+        backgroundColor:colors.primary,
+        height:50
+    },
+});
