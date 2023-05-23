d0b73cdc67b2e9f0232222735039f6878f10e386
kchaou-fakhri <kchaoufakhri@gmail.com>
2023-05-23 18:06:31 +0100

    feat() : Login with new architecture - implement login ui - implement translate feat #feature/FRNP-3/login_with_new_architecture

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigation from './src/navigation/configuration/rootNavigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import colors from './src/assets/colors';
 
 
 const store = reduxStore();
@@ -17,6 +18,7 @@ export default function App() {
   return (
   <Provider store={store}>
 
+<StatusBar backgroundColor={colors.primary} />
     <SafeAreaProvider style={{flex : 1}}>
       <NavigationContainer>
         <RootNavigation />
