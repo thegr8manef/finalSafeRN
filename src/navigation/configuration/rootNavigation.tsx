fe5279fc2750d5b2816ff077ffe8913538f9d41d
Fedi Sarray <fsarray@mobelite.com>
2023-08-01 02:37:08 +0100

    test(statistic): card one component

--- src/navigation/configuration/rootNavigation.tsx
+++ src/navigation/configuration/rootNavigation.tsx
@@ -4,10 +4,10 @@ import {
   createStackNavigator,
 } from '@react-navigation/stack';
 import {StackParamList} from './navigation.types';
-import {LoginPage} from '../../profileContext/adapters/primaries/login';
 import {ProfilePage} from '../../profileContext/adapters/primaries/Profile';
 import {HomeNavigation} from '../drawer/homeNavigation';
-import {SplashPage} from '../../profileContext/adapters/primaries/Splash';
+import { LoginPage } from '../../profileContext/adapters/primaries/login';
+import { SplashPage } from '../../profileContext/adapters/primaries/Splash';
 const Stack = createStackNavigator<StackParamList>();
 
 export default class RootNavigation extends PureComponent {
