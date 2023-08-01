4811267579ca5b42ccba3dcc2bd91060212cec7c
Fedi Sarray <fsarray@mobelite.com>
2023-08-01 02:38:07 +0100

    test(statistic): header dashboard component

---
+++ __tests__/src/statisticContext/presentation/components/HeaderDashbord.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react-native';
+import { HeaderDashboard } from '../../../../../src/statisticContext/adapters/primaries/components/HeaderDashboard';
+
+describe('HeaderDashboard', () => {
+    it('should render the component with the correct text', () => {
+        const { getByText } = render(<HeaderDashboard children={''} visits={11} dateDebut={''} dateFinale={''} labelPerimetre={''} numberChantier={10} navigation={undefined} />);
+        expect(getByText('Mes visites :11')).toBeTruthy();
+        expect(getByText('10')).toBeTruthy();
+    });
+});
