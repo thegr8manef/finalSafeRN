d910311ffb9b8a9389fc0b6eb8bd70aa8dc4b63f
Fedi Sarray <fsarray@mobelite.com>
2023-08-11 14:42:41 +0100

    feat(visitContext_test): divider visite

---
+++ __tests__/visiteContext/presentation/components/dividerVisite.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import { DividerVisite } from '../../../../src/visiteContext/adapters/primaries/components/DividerVisite';
+
+describe('DividerVisite', () => {
+  it('should render the component correctly', () => {
+    const {getByTestId} = render(<DividerVisite />);
+    expect(getByTestId('divider')).toBeTruthy();
+  });
+});
