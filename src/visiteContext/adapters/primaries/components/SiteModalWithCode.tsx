89c87a8aab51989935db71cbe17e671e67471f57
Fedi Sarray <fsarray@mobelite.com>
2023-08-11 14:44:25 +0100

    feat(visitContext_test): site modal with code

--- src/visiteContext/adapters/primaries/components/SiteModalWithCode.tsx
+++ src/visiteContext/adapters/primaries/components/SiteModalWithCode.tsx
@@ -1,13 +1,12 @@
-import {View, Text, Modal, StyleSheet, ActivityIndicator} from 'react-native';
-import React, {useState, useEffect} from 'react';
+import { View, Text, Modal, StyleSheet, TextInput, ActivityIndicator, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import colors from '../../../../assets/colors';
-import {useTranslation} from 'react-i18next';
-import {TextInput} from 'react-native-gesture-handler';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
   modalVisible: boolean;
   setWithCodeVisibilty(visibilty: boolean): void;
-  code: string;
+  code?: string;
   setCode: (CodeChantier: string) => void;
   setclicked: (clicked: boolean) => void;
   clicked: boolean;
@@ -16,9 +15,9 @@ interface Props {
 }
 
 export const SiteModalWithCode = (props: Props) => {
-  const {t} = useTranslation();
-  const SetVisibilty = (visibilty : boolean) => {
-props.setWithCodeVisibilty(visibilty)
+  const { t } = useTranslation();
+  const SetVisibilty = (visibilty: boolean) => {
+    props.setWithCodeVisibilty(visibilty)
   }
   return (
     <Modal
@@ -28,31 +27,33 @@ props.setWithCodeVisibilty(visibilty)
       <View style={styles.centeredView}>
         <View style={styles.header}>
           <Text
+            testID='cancel-modal-btn'
             style={styles.normalText}
             onPress={() => {
               [SetVisibilty(false)];
             }}>
             {t('txt_cancel')}
           </Text>
-          <Text style={[styles.normalText, {fontWeight: 'bold', fontSize: 15}]}>
+          <Text style={[styles.normalText, { fontWeight: 'bold', fontSize: 15 }]}>
             {t('txt.code.chantier.no.star')}
           </Text>
           <Text
+            testID='submit-btn'
             style={styles.normalText}
             onPress={() => {
               // props.code.length !== 0
               //   ? [props.setclicked(true), (props.clicked = true),{props.loading === false ? alert(t('this code isn't exist')) : [props.setWithCodeVisibilty(false)] }]
               //   : alert(t('error.point.empty'));
-              if (props.code.length !== 0) {
+              if (props?.code?.length !== 0) {
                 props.setclicked(true);
                 props.clicked = true;
                 if (props.codeExist === undefined) {
-                  alert(t('no_cs_by_ref'));
+                  Alert.alert(t('no_cs_by_ref'));
                 } else {
                   SetVisibilty(false);
                 }
               } else {
-                alert(t('error.point.empty'));
+                Alert.alert(t('error.point.empty'));
               }
             }}>
             {t('txt.valider')}
@@ -61,16 +62,17 @@ props.setWithCodeVisibilty(visibilty)
         <View style={styles.container}>
           <Text>{t('txt.code.chantier')}</Text>
           <TextInput
+            testID='code-input'
             cursorColor={colors.primary}
-            value={props.code.toUpperCase()}
+            value={props.code?.toUpperCase()}
             onChangeText={CodeChantier => props.setCode(CodeChantier)}
           />
-          <View style={{height: 1, backgroundColor: colors.primary}}></View>
+          <View style={{ height: 1, backgroundColor: colors.primary }}></View>
         </View>
         <ActivityIndicator
           size="large"
           color={colors.black}
-          style={{display: !props.loading ? 'flex' : 'none'}}
+          style={{ display: !props.loading ? 'flex' : 'none' }}
         />
       </View>
     </Modal>
