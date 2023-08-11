0c1729b8d571c0b8fe0dc158acc053b8a8d69df8
Fedi Sarray <fsarray@mobelite.com>
2023-08-11 14:39:48 +0100

    feat(visitContext_test): observation positive

--- src/visiteContext/adapters/primaries/components/ObservationPositiveOFF.tsx
+++ src/visiteContext/adapters/primaries/components/ObservationPositiveOFF.tsx
@@ -1,22 +1,26 @@
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import colors from '../../../../assets/colors';
 import React from 'react';
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
-  onPressPositiveOFF: void;
+  onPressPositiveOFF: () => void;
 }
 export const OPOFF = (props: Props) => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
-    <TouchableOpacity onPress={props.onPressPositiveOFF} style={{flex: 1}}>
-      <View style={{flex: 2}}>
+    <TouchableOpacity
+      testID='up-thumb-btn'
+      onPress={props.onPressPositiveOFF} 
+      style={{ flex: 1 }}>
+      <View style={{ flex: 2 }}>
         <Image
+          testID='img'
           source={require('../../../../assets/img/icn_positive_disabled_blocked.png')}
           style={styles.logoImage1}
         />
       </View>
-      <View style={{flex: 1}}>
+      <View style={{ flex: 1 }}>
         <Text style={styles.TextObservation}>{t('observation_positive')}</Text>
       </View>
     </TouchableOpacity>
