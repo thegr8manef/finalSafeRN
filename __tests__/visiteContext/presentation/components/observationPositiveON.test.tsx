7b7ce76834d6c38465938f8a34af433dcb8c385f
Fedi Sarray <fsarray@mobelite.com>
2023-08-11 14:39:48 +0100

    feat(visitContext_test): observation positive

---
+++ __tests__/visiteContext/presentation/components/observationPositiveON.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import { OPON } from '../../../../src/visiteContext/adapters/primaries/components/ObservationPositiveON';
+import i18n from '../../../../src/assets/languages/i18n';
+
+describe('ObservationPositiveON test', () => {
+    const mockOnPressPositive = jest.fn();
+
+    beforeEach(() => {
+        i18n.init();
+    });
+
+    it('renders correctly', () => {
+        const { getByTestId } = render(
+            <OPON onPressPositive={mockOnPressPositive} />
+        );
+
+        const upThumbButton = getByTestId('up-thumb-btn');
+        expect(upThumbButton).toBeDefined();
+
+        const image = getByTestId('img')
+        expect(image).toBeDefined();
+
+    });
+
+    it('calls onPressPositive when button is pressed', () => {
+        const { getByTestId } = render(
+            <OPON onPressPositive={mockOnPressPositive} />
+        );
+
+        const upThumbButton = getByTestId('up-thumb-btn');
+        fireEvent.press(upThumbButton);
+
+        expect(mockOnPressPositive).toHaveBeenCalledTimes(1);
+    });
+
+});
