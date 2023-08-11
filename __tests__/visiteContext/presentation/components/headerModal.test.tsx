ae2a06ed5583e609c513aec3c54c9a7d36f8cef6
Fedi Sarray <fsarray@mobelite.com>
2023-08-11 14:42:56 +0100

    feat(visitContext_test): header modal

---
+++ __tests__/visiteContext/presentation/components/headerModal.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { HeaderModal } from '../../../../src/visiteContext/adapters/primaries/components/HeaderModal';
+import i18n from '../../../../src/assets/languages/i18n';
+
+describe('HeaderModal', () => {
+    const props = {
+        onPressCustomizeNegative: jest.fn(),
+        onPressCustomizePositive: jest.fn(),
+        children: ''
+    }
+    beforeEach(() => {
+        i18n.init();
+    });
+    it('should render the component correctly', () => {
+        const { getByTestId } = render(<HeaderModal {...props} />);
+        expect(getByTestId('cancel-btn')).toBeTruthy();
+        expect(getByTestId('confirm-btn')).toBeTruthy();
+    });
+
+    it('should render the children correctly', () => {
+        const { getByTestId } = render(<HeaderModal {...props} children="This is the header" />);
+        expect(getByTestId('header')).toBeTruthy();
+    });
+});
