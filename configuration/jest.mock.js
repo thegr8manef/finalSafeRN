3cee0548815b729ff65b6d07794fc416f47793af
Fedi Sarray <fsarray@mobelite.com>
2023-08-11 14:33:02 +0100

    fix(mock): add i18n & react native radio grp

--- configuration/jest.mock.js
+++ configuration/jest.mock.js
@@ -1,3 +1,25 @@
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+
+  useTranslation: () => {
+    return {
+      t: str => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  },
+}));
+
+
+jest.mock('react-native-radio-buttons-group', () => ({
+  RadioGroup: () => ({default: jest.fn()}),
+}));
 
 
 jest.mock('react-native-chart-kit', () => ({
