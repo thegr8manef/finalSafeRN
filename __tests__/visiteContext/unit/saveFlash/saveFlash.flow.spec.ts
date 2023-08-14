b3380133f46b11b89a9a82f662ed2f62473fadbf
Fedi Sarray <fsarray@mobelite.com>
2023-08-14 12:08:01 +0100

    feat(visitContext_test): save flash

---
+++ __tests__/visiteContext/unit/saveFlash/saveFlash.flow.spec.ts
@@ -0,0 +1,69 @@
+import { Store } from "redux"
+import { ReduxStoreWO } from "../../../reduxStore.wo"
+import { AppState } from "../../../../src/redux_configuration/appState"
+import { Flash } from "../../../../src/visiteContext/domain/entity/Flash"
+import { Photo } from "../../../../src/visiteContext/domain/entity/Photo"
+import { saveFashErrorSelector, saveFlashLoadingSelector } from "../../../../src/visiteContext/useCases/saveFlash/selectors"
+import { SaveFlash, SaveFlashFailed, SaveFlashSuccess } from "../../../../src/visiteContext/useCases/saveFlash/action"
+
+const deepFreeze = require('deep-freeze')
+
+describe("Save flash Context flow", () => {
+
+    let store: Store<AppState>
+    let reduxStoreWO: ReduxStoreWO
+    let flash: Flash
+
+    beforeEach(() => {
+        reduxStoreWO = new ReduxStoreWO()
+        store = reduxStoreWO.getStore()
+        deepFreeze(store.getState())
+        flash = new Flash(
+            '1234567890',
+            [new Photo(),new Photo()],
+            1,
+        );
+    })
+
+    it("should have initial state", () => {
+        expect(saveFlashLoadingSelector(store.getState())).toBeFalsy()
+        expect(saveFashErrorSelector(store.getState())).toBeUndefined()
+    })
+
+    it('should be start save flash visit', async () => {
+      
+        await store.dispatch(SaveFlash(flash))
+        
+        await expect(saveFlashLoadingSelector(store.getState())).toBeTruthy()
+        await expect(saveFashErrorSelector(store.getState())).toBeUndefined()
+   
+    })
+
+    it('should be save flash visit success', (done) => {
+        let eventCounter = 0
+        store.subscribe(() => {
+            if (++eventCounter === 1) {
+                expect(saveFlashLoadingSelector(store.getState())).toBeFalsy()
+                expect(saveFashErrorSelector(store.getState())).toBeUndefined()
+                done()
+            }
+        })
+        store.dispatch(SaveFlashSuccess())
+        reduxStoreWO.saveFlashNext(flash)
+    })
+
+    it('should return error while save flash visit failed', (done) => {
+        let eventCounter = 0
+        store.subscribe(() => {
+            if (++eventCounter === 1) {
+                expect(saveFlashLoadingSelector(store.getState())).toBeFalsy()
+                expect(saveFashErrorSelector(store.getState())).toBe('ERROR')
+                done()
+            }
+        })
+        store.dispatch(SaveFlashFailed('ERROR'))
+        reduxStoreWO.saveDataError('error')
+        
+    })
+
+})
