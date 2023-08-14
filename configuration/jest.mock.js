a0e2082452b1b4c7aebd0e598bc66578056e7319
Fedi Sarray <fsarray@mobelite.com>
2023-08-14 16:28:41 +0100

    fix(mock): add  rn-image-picker

--- configuration/jest.mock.js
+++ configuration/jest.mock.js
@@ -16,12 +16,32 @@ jest.mock('react-i18next', () => ({
   },
 }));
 
-
 jest.mock('react-native-radio-buttons-group', () => ({
   RadioGroup: () => ({default: jest.fn()}),
 }));
 
-
 jest.mock('react-native-chart-kit', () => ({
   PieChart: jest.fn(),
+}));
+
+jest.mock('react-native//Libraries/PermissionsAndroid/PermissionsAndroid', () =>
+  require('react-native//Libraries/PermissionsAndroid/PermissionsAndroid/mock'),
+);
+
+// Simulate the response from the camera
+const mockImageResponse = {
+  didCancel: false,
+  assets: [{uri: 'mock_image_uri'}],
+};
+const mockLaunchCamera = jest.fn((_, callback) => {
+  callback(mockImageResponse);
+});
+
+const mockLaunchImageLibrary = jest.fn((_, callback) => {
+  callback(mockImageResponse);
+});
+
+jest.mock('react-native-image-picker', () => ({
+  launchCamera: mockLaunchCamera,
+  launchImageLibrary: mockLaunchImageLibrary,
 }));
