00636fdcb38555d8d712d07566e37e7898872511
khbeieb <khbeieb@mobelite.fr>
2023-08-22 10:21:26 +0100

    fixed catch for asynchronous errors in a promise

--- src/contexts/profileContext/adapters/secondaires/DBUserRepository.ts
+++ src/contexts/profileContext/adapters/secondaires/DBUserRepository.ts
@@ -1,36 +1,32 @@
-import {Observable, from} from 'rxjs';
-import {UserRepository} from '../../domain/gateway/userReposiory';
+import { Observable, from } from 'rxjs';
+import { UserRepository } from '../../domain/gateway/userReposiory';
 import ApplicationContext from '@common/appConfig/ApplicationContext';
-import {Profile} from '../../domain/entity/profile';
-import {LocalProfilMapper} from './mapper/localProfile.mapper';
-import {User} from '../../domain/entity/user';
+import { Profile } from '../../domain/entity/profile';
+import { LocalProfilMapper } from './mapper/localProfile.mapper';
+import { User } from '../../domain/entity/user';
 export class DBUserRepository implements UserRepository {
   setUserConnected(userConnected: Profile): Observable<void> {
     const promiSetUser = new Promise<void>((resolve, reject) => {
       const db = ApplicationContext.getInstance().db();
-
-      try {
-        db.then(realm => {
-          realm?.write(() => {
-            realm.create('User', {
-              fn: userConnected.name.substring(
-                0,
-                userConnected.name.indexOf(' '),
-              ),
-              ln: userConnected.name.substring(userConnected.name.indexOf(' ')),
-              em: userConnected.email,
-              connected: true,
-              lr: false,
-              visitCreated: 0,
-              lu: '-1',
-              token: userConnected.accessToken,
-            });
+      db.then(realm => {
+        realm?.write(() => {
+          realm.create('User', {
+            fn: userConnected.name.substring(
+              0,
+              userConnected.name.indexOf(' '),
+            ),
+            ln: userConnected.name.substring(userConnected.name.indexOf(' ')),
+            em: userConnected.email,
+            connected: true,
+            lr: false,
+            visitCreated: 0,
+            lu: '-1',
+            token: userConnected.accessToken,
+            primaryKey: 'id'
           });
-          resolve(); // Emit the boolean value
         });
-      } catch (error) {
-        reject(error);
-      }
+        resolve(); // Emit the boolean value
+      }).catch((error) => reject(error));
     });
 
     return from(promiSetUser);
@@ -40,21 +36,18 @@ export class DBUserRepository implements UserRepository {
     const promiseCheckUser = new Promise<boolean>((resolve, reject) => {
       const db = ApplicationContext.getInstance().db();
 
-      try {
-        db.then(realm => {
-          realm?.write(() => {
-            const objects = realm.objects('User');
-            if (objects.length > 0) {
-              const connected = objects[0].connected;
-              resolve(connected);
-            } else {
-              resolve(false);
-            }
-          });
+      db.then(realm => {
+        realm?.write(() => {
+          const objects = realm.objects('User');
+          if (objects.length > 0) {
+            const connected = objects[0].connected;
+            resolve(connected);
+          } else {
+            resolve(false);
+          }
         });
-      } catch (error) {
-        reject(error);
-      }
+      }).catch(error => reject(error)
+      );
     });
     return from(promiseCheckUser);
   }
@@ -63,16 +56,12 @@ export class DBUserRepository implements UserRepository {
     const db = ApplicationContext.getInstance().db();
 
     const promiseCheckUser = new Promise<Profile>((resolve, reject) => {
-      try {
-        db.then(realm => {
-          realm?.write(() => {
-            const objects = realm.objects('User');
-            resolve(LocalProfilMapper.mapUserDbToProfile(objects[0]));
-          });
+      db.then(realm => {
+        realm?.write(() => {
+          const objects = realm.objects('User');
+          resolve(LocalProfilMapper.mapUserDbToProfile(objects[0]));
         });
-      } catch (error) {
-        reject(error);
-      }
+      }).catch(error => reject(error));
     });
     return from(promiseCheckUser);
   }
@@ -80,17 +69,13 @@ export class DBUserRepository implements UserRepository {
   updateLocalProfile(user: User): Observable<void> {
     const promiUpdateUser = new Promise<void>((resolve, reject) => {
       const db = ApplicationContext.getInstance().db();
-      try {
-        db.then(realm => {
-          const updt = realm.objects('User');
-          realm?.write(() => {
-            updt[0].rg = user.region;
-          });
-          resolve();
+      db.then(realm => {
+        const updt = realm.objects('User');
+        realm?.write(() => {
+          updt[0].rg = user.region;
         });
-      } catch (error) {
-        reject(error);
-      }
+        resolve();
+      }).catch(error => reject(error));
     });
 
     return from(promiUpdateUser);
