137d9d85774454271f1ad31ed530fed8a11a16a4
khbeieb <khbeieb@mobelite.fr>
2023-08-24 15:41:25 +0100

    fixed error handling

--- src/contexts/synchronisationContext/adapters/secandaires/DBSynchronisationRepository.ts
+++ src/contexts/synchronisationContext/adapters/secandaires/DBSynchronisationRepository.ts
@@ -8,25 +8,21 @@ export class DBSynchronisationRepository implements SynchronisationRepository {
   saveData(sites: Site[]): Observable<void> {
     const promisSaveData = new Promise<void>((resolve, reject) => {
       const db = ApplicationContext.getInstance().db();
-      try {
-        if (sites.length > 0) {
-          db.then(realm => {
-            const updt = realm.objects('User');
-            realm?.write(() => {
-              sites.forEach(site => {
-                realm.create(
-                  'Chantier',
-                  SynchronisationMapper.mapSiteToChantier(site),
-                );
-              });
-              updt[0].lu = sites[0].last_update?.toString();
+      if (sites.length > 0) {
+        db.then(realm => {
+          const updt = realm.objects('User');
+          realm?.write(() => {
+            sites.forEach(site => {
+              realm.create(
+                'Chantier',
+                SynchronisationMapper.mapSiteToChantier(site),
+              );
             });
-          }).catch(error => reject(error));
-        }
-        resolve();
-      } catch (error) {
-        reject(error);
+            updt[0].lu = sites[0].last_update?.toString();
+          });
+        }).catch(error => reject(error));
       }
+      resolve();
     });
 
     return from(promisSaveData);
@@ -35,14 +31,10 @@ export class DBSynchronisationRepository implements SynchronisationRepository {
   loadLastUpdateDate(): Observable<string> {
     const promisLastUpdate = new Promise<string>((resolve, reject) => {
       const db = ApplicationContext.getInstance().db();
-      try {
-        db.then(realm => {
-          const objects = realm.objects('User');
-          resolve(objects[0].lu);
-        });
-      } catch (error) {
-        reject(error);
-      }
+      db.then(realm => {
+        const objects = realm.objects('User');
+        resolve(objects[0].lu);
+      }).catch(error => reject(error));
     });
 
     return from(promisLastUpdate);
