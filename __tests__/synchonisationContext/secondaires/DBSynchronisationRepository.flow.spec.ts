437c48f9435444c8a6f9a7570203267587613b61
khbeieb <khbeieb@mobelite.fr>
2023-08-24 15:42:29 +0100

    saveData Done

--- __tests__/synchonisationContext/secondaires/DBSynchronisationRepository.flow.spec.ts
+++ __tests__/synchonisationContext/secondaires/DBSynchronisationRepository.flow.spec.ts
@@ -3,52 +3,39 @@ import { firstValueFrom } from "rxjs";
 import { Site } from "@contexts/visiteContext/domain/entity/Site";
 import { DBSynchronisationRepository } from "@contexts/synchronisationContext/adapters/secandaires/DBSynchronisationRepository";
 import { User } from "@common/adapters/secondaries/db/entity/User";
-import { SynchronisationMapper } from "@contexts/synchronisationContext/adapters/secandaires/mapper/synchronisationMapper";
-import Realm from "../../../configuration/mocks/realm.mock";
+import Realm from "realm";
 import { Chantier } from "@common/adapters/secondaries/db/entity/Chantier";
 import { User as UserSchema } from "@common/adapters/secondaries/db/entity/User";
 import { Profile } from "@contexts/profileContext/domain/entity/profile";
+import { Zone } from "@common/adapters/secondaries/db/entity/Zone";
+import { Remarque } from "@common/adapters/secondaries/db/entity/Remarque";
+import { Photo } from "@common/adapters/secondaries/db/entity/Photo";
+import { Accompagnant } from "@common/adapters/secondaries/db/entity/Accompagnant";
+import { Statistic } from "@common/adapters/secondaries/db/entity/Statistic";
+import { StatisticRisk } from "@common/adapters/secondaries/db/entity/StatisticRisk";
+import { StatisticVisit } from "@common/adapters/secondaries/db/entity/StatisticVisit";
+import { StatisticObservation } from "@common/adapters/secondaries/db/entity/StatisticObservation";
+import { StatisticUser } from "@common/adapters/secondaries/db/entity/StatisticUser";
+import { StatisticRiskObject } from "@common/adapters/secondaries/db/entity/StatisticRiskObject";
 
 jest.mock("@common/appConfig/ApplicationContext");
 
 describe("DBSynchronisationRepository Tests", () => {
     let synchronisationRepository: DBSynchronisationRepository
-    let mockRealmInstance = new Realm({ schema: [User, Chantier] });
+    const realmInstance = new Realm({ schema: [UserSchema, Chantier, Zone, Remarque, Photo, Accompagnant, Statistic, StatisticRisk, StatisticVisit, StatisticObservation, StatisticUser, StatisticRiskObject] });
     let userSchema: UserSchema
     let userConnected: Profile
 
-    const newChantier = mockRealmInstance.create('Chantier', {
-        id: '123',
-        no: 'Chantier 123',
-        ad: '123 Main Street',
-        type: 1,
-        ac: true,
-        cp: '12345',
-        co: 'Company',
-        py: 'Country',
-        vl: 'City',
-        sr: 5,
-        cd: 'Category',
-        st: 2,
-        lu: 1630000000,
-        remarques: [], // Array of Remarque instances
-        ref: 'Reference123',
-        org: 'Organization',
-        ol_name: "Owner's Name",
-        osc: 'OSC123',
-        pid: 'ProjectID',
-        piid: 'ProjectItemID',
-    });
-    const sites: Site[] = [new Site('1', 'name', 'rue', 1, false, '1235', 'FR', 'Paris', -1, 123, 'ref', 'NR', 'r', '11', 'gg', 1, '', ''),
-    new Site('12', 'name 11', 'rue', 1, false, '1235', 'FR', 'Paris', -1, 456, 'ref', 'NR', 'r', '11', 'gg', 1, '', '')]
-
+    const sites: Site[] = [new Site('1', 'name', 'rue', 1, false, '1235', 'FR', 'Paris', -1, 123, 'ref', 'NR', 'r', '11', 'gg', 1, 1, ''),
+    new Site('12', 'name 11', 'rue', 1, false, '1235', 'FR', 'Paris', -1, 456, 'ref', 'NR', 'r', '11', 'gg', 1, 1, '')]
 
     beforeEach(() => {
         synchronisationRepository = new DBSynchronisationRepository();
 
         (ApplicationContext.getInstance as jest.Mock).mockReturnValue({
-            db: jest.fn(() => Promise.resolve(mockRealmInstance))
+            db: jest.fn(() => Promise.resolve(realmInstance))
         });
+
         userConnected = new Profile(
             '123',
             'John Doe',
@@ -57,7 +44,9 @@ describe("DBSynchronisationRepository Tests", () => {
             'DR EIC TESTS',
             '-1',
         )
+
         userSchema = {
+            id: "123",
             fn: userConnected.name.substring(
                 0,
                 userConnected.name.indexOf(' '),
@@ -69,43 +58,30 @@ describe("DBSynchronisationRepository Tests", () => {
             visitCreated: 0,
             lu: '-1',
             token: userConnected.accessToken,
-            primaryKey: 'id',
         }
     });
 
-    afterEach(() => {
-        jest.clearAllMocks();
+    afterAll(() => {
+        realmInstance.write(() => {
+            realmInstance.deleteAll();
+        });
+    })
+
+    it("should save site data correctly and update the 'lastUpdate (lu)' of the user", async () => {
+        realmInstance.write(() => {
+            realmInstance.create('User', userSchema);
+        });
+
+        await firstValueFrom(synchronisationRepository.saveData(sites))
+
+        const chantierCollection = realmInstance.objects('Chantier');
+        expect(chantierCollection.length).toEqual(sites.length);
+
+        const retrievedUser: User = realmInstance.objectForPrimaryKey("User", userConnected.id)
+        expect(retrievedUser.lu).toEqual(sites[0].last_update.toString())
     });
 
-    it('should create and retrieve objects using MockRealm', () => {
-        mockRealmInstance.write(() => {
-            const createdObject = mockRealmInstance.create('Chantier', newChantier);
-            const retrievedObject = mockRealmInstance.objectForPrimaryKey('Chantier', createdObject.id);
-            expect(retrievedObject).toEqual(createdObject);
-        });
-    });
-
-    it("should save site data correctly", async () => {
-        const site1: Site = sites[0];
-        const site2: Site = sites[1];
-        mockRealmInstance.write(() => {
-            mockRealmInstance.create('User', userSchema);
-        });
-        mockRealmInstance.write(() => {
-            sites.forEach((site: Site) => {
-                mockRealmInstance.create('Chantier', SynchronisationMapper.mapSiteToChantier(site));
-            })
-        });
-
-        await firstValueFrom(synchronisationRepository.saveData(sites)) 
-        const retrievedUser:User = mockRealmInstance.objectForPrimaryKey("User",userConnected.primaryKey)
-        mockRealmInstance.write(() => {
-            mockRealmInstance.create('User', retrievedUser);
-        });
-        expect(retrievedUser.lu).toEqual(site1.last_update.toString())
-      });
-
-      it('should handle errors in saveData', async() => {
+    it('should handle errors in saveData', async () => {
         const errorMessage = "Mocked error";
 
         (ApplicationContext.getInstance as jest.Mock).mockReturnValue({
@@ -113,12 +89,12 @@ describe("DBSynchronisationRepository Tests", () => {
         });
 
         try {
-            await firstValueFrom(synchronisationRepository.saveData(sites)) 
+            await firstValueFrom(synchronisationRepository.saveData(sites))
         } catch (error: any) {
             expect(error.message).toBe(errorMessage)
         }
-      })
-    });
+    })
+});
 
 
 
