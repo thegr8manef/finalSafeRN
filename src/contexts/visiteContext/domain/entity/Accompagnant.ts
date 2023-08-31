fc1a321e730065cfa83dcd3151ae67c89d706f62
Amira <adgham@mobelite.fr>
2023-08-31 19:50:18 +0200

    add accompagnant mapper logic

--- src/contexts/visiteContext/domain/entity/Accompagnant.ts
+++ src/contexts/visiteContext/domain/entity/Accompagnant.ts
@@ -1,12 +1,44 @@
 
 
-export default class Accompagnant {
-    constructor (
-        private _Accompagnant   : string,
-        private _created_At : string,
-        private _description: string,
-        private _level      : number,
-        private _note      : number,
-    ){}
+export class Accompagnant {
+
+    constructor(
+        private _ac: boolean,
+        private _em: string,
+        private _fn: string,
+        private _id: number,
+        private _ln: string,
+    ) { }
+
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+    public get fn(): string {
+        return this._fn;
+    }
+    public set fn(value: string) {
+        this._fn = value;
+    }
+    public get em(): string {
+        return this._em;
+    }
+    public set em(value: string) {
+        this._em = value;
+    }
+    public get ac(): boolean {
+        return this._ac;
+    }
+    public set ac(value: boolean) {
+        this._ac = value;
+    }
+    public get ln(): string {
+        return this._ln;
+    }
+    public set ln(value: string) {
+        this._ln = value;
+    }
 
 }
