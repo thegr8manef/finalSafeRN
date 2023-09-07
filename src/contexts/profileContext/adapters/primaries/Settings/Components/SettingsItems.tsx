93badb049981601324511c977d74b19adde38eac
thegr8manef <jedidimanef@gmail.com>
2023-09-07 11:06:11 +0100

    Create Settings Screen

---
+++ src/contexts/profileContext/adapters/primaries/Settings/Components/SettingsItems.tsx
@@ -0,0 +1,39 @@
+import { View, Text, StyleSheet, Image, ImageSourcePropType } from 'react-native';
+import React from 'react';
+import * as utils from '@utils/index';
+import { ItemListHeader } from './itemListHeader';
+import { ItemListContent } from './itemListContent';
+
+interface Props {
+title : string;
+topSubtitle : string;
+bottomSubtitme : string;
+lastUpdateDate : string;
+iconTop: ImageSourcePropType[];
+iconBottom : ImageSourcePropType[];
+notNull : boolean;
+style : string[];
+style_bottom : string[];
+isFirstItem : boolean;
+onClickFirstUrl () : void;
+onClickSecondUrl () : void;
+}
+export const SettingsItems = (props: Props): JSX.Element =>{
+
+  
+    return (
+    <View style={{flex:1}}>
+    <ItemListHeader title={props.title} isFirstItem={props.isFirstItem} />
+    <ItemListContent Subtitle={props.topSubtitle} lastUpdateDate={props.lastUpdateDate} icon={props.iconTop} style_image={props.style} notNull={props.notNull} onSelect={props.onClickFirstUrl} />
+    <ItemListContent Subtitle={props.bottomSubtitme} lastUpdateDate={props.lastUpdateDate} icon={props.iconBottom } style_image={props.style_bottom} notNull={props.notNull} onSelect={props.onClickSecondUrl} />
+    </View>
+    );
+  };
+const styles = StyleSheet.create({
+    title:{
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: utils.colors.black
+    },
+
+})
--- src/contexts/profileContext/adapters/primaries/Settings/settings.container.tsx
+++ src/contexts/profileContext/adapters/primaries/Settings/settings.container.tsx
@@ -1,16 +1,14 @@
-import { View, Image, Text, ScrollView, StyleSheet, Linking } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import { View, ScrollView, StyleSheet, Linking } from 'react-native';
+import React, { useState } from 'react';
 import { SettingsAppInfo } from './Components/SettingsAppInfo';
-import * as utils from '@utils/index';
 import { useTranslation } from 'react-i18next';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { StackParamList } from '@navigConfig/navigation.types';
 import { Visit } from '@contexts/visiteContext/domain/entity/Visit';
 import { Profile } from '@contexts/profileContext/domain/entity/profile';
-import { SettingsItemsGroup  } from './Components/SettingsItemsGroup';
-import { SettingsItem } from './Components/SettingsItem';
 import { LanguageModal } from './Components/languageModal';
 import { URL_NOTICE, URL_POLICY } from '@common/constants';
+import { SettingsItemsGroup } from './Components/SettingsItemsGroup';
 
 interface Props {
     navigation: Partial<StackNavigationProp<StackParamList>>;
@@ -25,10 +23,15 @@ interface Props {
 export const SettingsContainer = (props: Props) => {
     const { t } = useTranslation();
     const [modalVisible, setModalVisible] = useState<boolean>(false);
+    var DateNow: string;
     const handlSynchronisation = () => {
         props.sendData(props.profile?.accessToken!!, props.profile?.lastUpdate!!, props.visits!! )
       }
-      const DateNow = Date().replace(/\sGMT.*/, '').toString();
+        if(props.profile?.lastUpdate!! === undefined){
+          DateNow = Date().replace(/\sGMT.*/, '').toString();
+        }else{
+          DateNow = props.profile?.lastUpdate!!
+        }
       const loadInBrowserPolicy = () => {
         Linking.openURL(URL_POLICY).catch(err => console.error("Couldn't load page", err));
       };
@@ -39,11 +42,7 @@ export const SettingsContainer = (props: Props) => {
     <View style={styles.f1}>
         <ScrollView style={styles.f1}>
           <SettingsAppInfo visits={props.visits} sendData={handlSynchronisation} lastUpdateDate={t('txt.last.update.at')+' '+DateNow} />
-          <SettingsItemsGroup  title={t('txt.chantiers')} lastUpdateDate={DateNow} topSubtitle={t('txt.mes.chantier')} bottomSubtitme={t('txt.autres.chantier')} notNull={true} iconTop={[utils.images.synchoLogo]} iconBottom ={[utils.images.arrow_right]} style={[styles.Image]} style_bottom={[styles.Image_arrow]} isFirstItem={true} onClickFirstUrl={() => handlSynchronisation()} onClickSecondUrl={() => console.log('other sites modal')}/>   
-          <SettingsItem title={t('txt.referentiel.listes')} Subtitle={t('txt.typology.risques.questions')} lastUpdateDate={DateNow} iconTop={[utils.images.synchoLogo]} style_image={styles.Image_syncho} notNull={true} isFirstItem={false} onSelect={() => handlSynchronisation()}/>
-          <SettingsItem title={t('txt.photos')} Subtitle={t('txt.no.photos.wait.send')} lastUpdateDate={''} iconTop={[utils.images.mediaSyncho]} style_image={styles.Image_camera} notNull={false} isFirstItem={false} onSelect={() => handlSynchronisation()}/>
-          <SettingsItem title={t('txt.language')} Subtitle={t('txt.language')} lastUpdateDate={''} iconTop={[]} style_image={styles.Image_camera} notNull={false} isFirstItem={false} onSelect={() =>setModalVisible(true)} />
-          <SettingsItemsGroup  title={t('settings_about')} lastUpdateDate={''} topSubtitle={t('settings_confidentiality')} bottomSubtitme={t('settings_legal_notice')} notNull={false} iconTop={[utils.images.arrow_right]} iconBottom ={[utils.images.arrow_right]} style={[styles.Image_arrow]} style_bottom={[styles.Image_arrow]} isFirstItem={false} onClickFirstUrl={() => loadInBrowserPolicy()} onClickSecondUrl={() =>loadInBrowserNotice()}/>
+          <SettingsItemsGroup lastUpdateDate={DateNow} OnSynchronize={() => handlSynchronisation()} OnClickSitesList={() => console.log('other sites modal')} OnClickLanguageModal={() =>setModalVisible(true)} OnClickFirstURL={() => loadInBrowserPolicy()} OnClickSecondURL={() =>loadInBrowserNotice()} />
           <LanguageModal visible={modalVisible} onClose={() => setModalVisible(false)} />
         </ScrollView>
     </View>
@@ -53,24 +52,4 @@ const styles = StyleSheet.create({
     f1: {
       flex: 1,
     },
-    Image_syncho:{
-      width: 30,
-      height: 30,
-      alignSelf: 'center'
-  },
-  Image_camera:{
-    width: 30,
-    height: 25,
-    alignSelf: 'center'
-},
-Image:{
-  width: 30,
-  height: 30,
-  alignSelf: 'center'
-},
-Image_arrow:{
-  width: 10,
-  height: 20,
-  alignSelf: 'center'
-},
   });
